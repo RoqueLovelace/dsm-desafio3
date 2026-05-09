5b52139ca1d200fbf560b70bc9b6c859f56198bf
RoqueLovelace <chrisdesanfrancisco@gmail.com>
2026-05-09 01:43:14 -0600

    Add item Detail

--- app/src/main/java/com/rodriguezcortez/desafio/view/DetailActivity.kt
+++ app/src/main/java/com/rodriguezcortez/desafio/view/DetailActivity.kt
@@ -1,21 +1,56 @@
 package com.rodriguezcortez.desafio.view
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.rodriguezcortez.desafio.R
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var imgDet: ImageView
+    private lateinit var tvTitDet: TextView
+    private lateinit var tvTipoDet: TextView
+    private lateinit var tvDescDet: TextView
+    private lateinit var btnAbrir: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        imgDet = findViewById(R.id.imgDet)
+        tvTitDet = findViewById(R.id.tvTitDet)
+        tvTipoDet = findViewById(R.id.tvTipoDet)
+        tvDescDet = findViewById(R.id.tvDescDet)
+        btnAbrir = findViewById(R.id.btnAbrir)
+
+        val titulo = intent.getStringExtra("titulo")
+        val desc = intent.getStringExtra("desc")
+        val tipo = intent.getStringExtra("tipo")
+        val img = intent.getStringExtra("img")
+        val link = intent.getStringExtra("link")
+
+        tvTitDet.text = titulo
+        tvTipoDet.text = tipo
+        tvDescDet.text = desc
+
+        Glide.with(this)
+            .load(img)
+            .into(imgDet)
+
+        btnAbrir.setOnClickListener {
+
+            val inte = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse(link)
+            )
+
+            startActivity(inte)
         }
     }
 }
